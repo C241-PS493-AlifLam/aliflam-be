b48cb3a73183ca485fb17499c937e43742be8d3b
Abe1904 <albert.nindityo@gmail.com>
2024-06-06 20:23:49 +0700

    login register api

--- handlers/dataHandler.js
+++ handlers/dataHandler.js
@@ -1,9 +1,13 @@
 const { Firestore } = require('@google-cloud/firestore');
+
+// Create a new instance of Firestore
 const firestore = new Firestore();
 
 const addData = async (request, h) => {
     const data = request.payload;
-    const docRef = firestore.collection('your-collection').doc('your-document');
+    // Here you're accessing a Firestore collection
+    const docRef = firestore.collection('users').doc('8PcNCKyrcYLcV66jdI6Q');
+    // Setting data to Firestore
     await docRef.set(data);
     return h.response('Document added successfully').code(201);
 };

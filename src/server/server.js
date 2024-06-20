45e9a035e04bf5e35fe0b74f0f23661c6d08b8c1
AbyNurangga <nurangga.aby@student.ub.ac.id>
2024-06-20 16:05:41 +0700

    change port

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ const JWT_SECRET = 'your_jwt_secret_key'; // Ganti dengan secret key Anda
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
+    port: 8080,
     host: 'localhost',
     routes: {
       cors: {
